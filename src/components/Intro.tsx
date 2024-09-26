ac021cb1e665be99bf5ef787ea2acdecf82aca29
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-26 15:25:21 +0100

    styles needed

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,10 @@
 const links: links = {
   Linux:
-    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.7/linux-callie-0.0.7x64.zip",
+    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.8/linux-callie-0.0.8x64.zip",
   Windows:
-    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.7/win-callie-0.0.7x64.zip",
+    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.8/win-callie-0.0.8x64.zip",
   MacOS:
-    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.7/mac-callie-0.0.7.zip",
+    "https://github.com/MikeyA-yo/callie/releases/download/v0.0.8/mac-callie-0.0.8.zip",
 };
 
 interface links {
@@ -33,21 +33,136 @@ export default function Intro() {
           </button>
         )}
       </div>
-      <Usage />
+      <div className="flex flex-col gap-6 justify-evenly items-center p-4">
+        <h1 className="text-3xl">Usages and Instructions:</h1>
+        <Usage />
+        <OpenDemo />
+        <Join />
+        <ChatDemo />
+      </div>
     </>
   );
 }
 function Usage() {
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div>
-          <h2>Simplicity</h2>
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl">
+            Simplicity, simple and easy to understand UI
+          </h2>
+          <p>
+            All in one place, schedule a meeting directly, join a meeting
+            easily, view upcoming meetings all in one place, no hard time spent
+            navigating<br /> Step 1: Open Camera. <br />{" "}
+            Step 2: Click Join Meeting.<br />Step 3: Enter room ID(Address) and
+            join room
+          </p>
         </div>
-        <div></div>
+        <div>
+          <img src="/callie.png" width={650} height={450} />
+        </div>
       </div>
     </>
   );
+}
+function OpenDemo() {
+  return (
+    <>
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl">Open Camera</h2>
+          <p>
+            It's very simple, easy and concise to open the camera, just click
+            open camera and you're good.<br />When the camera is open you should
+            see something like this.{" "}
+            <br />It's essential to open the camera before joining a meeting, if
+            you don't want to be seen in the meeting, scroll down for
+            instructions.
+          </p>
+        </div>
+        <div>
+          <img src="/Callie-cam.png" width={650} height={450} />
+        </div>
+      </div>
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-xl">Turn Video Off</h2>
+          <p>
+            Just click on the icon below indicated by the blue arrow, Now you
+            should be able to go ahead and join a meeting while your video is
+            off.<br />You're expected to see this when you do that.{" "}
+            <br />Note that you can't join any meeting if you hit close camera,
+            and if you're in a meeting and you hit close camera you'd exit the
+            meeting
+          </p>
+        </div>
+        <div>
+          <img src="/callie-cam-off.png" width={650} height={450} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Join() {
+  return (
+    <>
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+      <div>
+        <h2 className="text-2xl">Join Meetings</h2>
+        <p>
+          When you click join meeting, the input field shows up, type in the
+          meeting ID of the meeting you are about to start/join then press Join
+          Room.
+          <br />
+          This room id is what the creator of the meeting chooses as his/her room id, it can be anything the person wants.
+          <br />
+          Once there are 2 or more participants in a room, the participants slides shows up.
+          You can double click on a certain user's video to see it in full screen
+        </p>
+      </div>
+      <div>
+        <img src="/callie-join.png" width={650} height={450} />
+      </div>
+    </div>
+    <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+      <div>
+        <h2 className="text-2xl">Joined Demo</h2>
+        <p>
+          Here you have clicked join room, and another user has also joined (i am meeting with myself in another window).
+          this is what it should look like.
+          <br />
+          The icons down, the mic and the cam, represent's the current state of the participant, to know if the participant is muted, or turns off his/her video.
+          
+          <br />
+          To leave the meeting, just click the red icon, which says leave room, that helps you exit
+        </p>
+      </div>
+      <div>
+        <img src="/callie-joined.png" width={650} height={450} />
+      </div>
+    </div>
+    </>
+  );
+}
+
+function ChatDemo(){
+  return (
+    <>
+     <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+       <div>
+        <h2 className="text-2xl">Chat Demo</h2>
+        <p>That's the chat box, where you can send messages to everyone in the meeting and also recieve messages in real time.
+          <br /> Sending messages is relatively easy, just typing your message and hit enter or click the send icon
+        </p>
+       </div>
+       <div>
+        <img src="/callie-chat-2.png" width={650} height={450} />
+       </div>
+     </div>
+    </>
+  )
 }
 function getOS() {
   const userAgent = window.navigator.userAgent;
@@ -59,6 +174,5 @@ function getOS() {
   else if (userAgent.indexOf("Linux") !== -1) os = "Linux";
   else if (/Android/.test(userAgent)) os = "Android";
   else if (/iPhone|iPad|iPod/.test(userAgent)) os = "iOS";
-
   return os;
 }
