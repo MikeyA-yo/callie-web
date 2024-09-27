137990639fc20b4166f2aae35ff00f8c4a215858
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-27 15:40:09 +0100

    sprinkle in some animations

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const links: links = {
   Linux:
     "https://github.com/MikeyA-yo/callie/releases/download/v0.0.8/linux-callie-0.0.8x64.zip",
@@ -20,16 +22,26 @@ export default function Intro() {
   return (
     <>
       <div className="flex flex-col min-h-screen gap-2 items-center justify-center">
-        <div className="text-xl font-mono text-center">
+        <motion.div
+          initial={{ opacity: 0, y: "-100%" }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="text-xl font-mono text-center"
+        >
           <p>Call and Chat live online.</p>
           <p>Easily make group calls with friends and loved ones.</p>
-        </div>
+        </motion.div>
         {condition && (
-          <button className="p-2 bg-[#31363F] font-sans">
+          <motion.button
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7 }}
+            className="p-2 bg-[#31363F] font-sans"
+          >
             <a href={links[os]} download={"Callie"}>
               Download for {os}
             </a>
-          </button>
+          </motion.button>
         )}
       </div>
       <div className="flex flex-col gap-6 justify-evenly items-center p-4">
@@ -53,9 +65,10 @@ function Usage() {
           <p>
             All in one place, schedule a meeting directly, join a meeting
             easily, view upcoming meetings all in one place, no hard time spent
-            navigating<br /> Step 1: Open Camera. <br />{" "}
-            Step 2: Click Join Meeting.<br />Step 3: Enter room ID(Address) and
-            join room
+            navigating
+            <br /> Step 1: Open Camera. <br /> Step 2: Click Join Meeting.
+            <br />
+            Step 3: Enter room ID(Address) and join room
           </p>
         </div>
         <div>
@@ -73,11 +86,11 @@ function OpenDemo() {
           <h2 className="text-2xl">Open Camera</h2>
           <p>
             It's very simple, easy and concise to open the camera, just click
-            open camera and you're good.<br />When the camera is open you should
-            see something like this.{" "}
-            <br />It's essential to open the camera before joining a meeting, if
-            you don't want to be seen in the meeting, scroll down for
-            instructions.
+            open camera and you're good.
+            <br />
+            When the camera is open you should see something like this. <br />
+            It's essential to open the camera before joining a meeting, if you
+            don't want to be seen in the meeting, scroll down for instructions.
           </p>
         </div>
         <div>
@@ -90,10 +103,11 @@ function OpenDemo() {
           <p>
             Just click on the icon below indicated by the blue arrow, Now you
             should be able to go ahead and join a meeting while your video is
-            off.<br />You're expected to see this when you do that.{" "}
-            <br />Note that you can't join any meeting if you hit close camera,
-            and if you're in a meeting and you hit close camera you'd exit the
-            meeting
+            off.
+            <br />
+            You're expected to see this when you do that. <br />
+            Note that you can't join any meeting if you hit close camera, and if
+            you're in a meeting and you hit close camera you'd exit the meeting
           </p>
         </div>
         <div>
@@ -108,60 +122,68 @@ function Join() {
   return (
     <>
       <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
-      <div>
-        <h2 className="text-2xl">Join Meetings</h2>
-        <p>
-          When you click join meeting, the input field shows up, type in the
-          meeting ID of the meeting you are about to start/join then press Join
-          Room.
-          <br />
-          This room id is what the creator of the meeting chooses as his/her room id, it can be anything the person wants.
-          <br />
-          Once there are 2 or more participants in a room, the participants slides shows up.
-          You can double click on a certain user's video to see it in full screen
-        </p>
+        <div>
+          <h2 className="text-2xl">Join Meetings</h2>
+          <p>
+            When you click join meeting, the input field shows up, type in the
+            meeting ID of the meeting you are about to start/join then press
+            Join Room.
+            <br />
+            This room id is what the creator of the meeting chooses as his/her
+            room id, it can be anything the person wants.
+            <br />
+            Once there are 2 or more participants in a room, the participants
+            slides shows up. You can double click on a certain user's video to
+            see it in full screen
+          </p>
+        </div>
+        <div>
+          <img src="/callie-join.png" width={650} height={450} />
+        </div>
       </div>
-      <div>
-        <img src="/callie-join.png" width={650} height={450} />
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+        <div>
+          <h2 className="text-2xl">Joined Demo</h2>
+          <p>
+            Here you have clicked join room, and another user has also joined (i
+            am meeting with myself in another window). this is what it should
+            look like.
+            <br />
+            The icons down, the mic and the cam, represent's the current state
+            of the participant, to know if the participant is muted, or turns
+            off his/her video.
+            <br />
+            To leave the meeting, just click the red icon, which says leave
+            room, that helps you exit
+          </p>
+        </div>
+        <div>
+          <img src="/callie-joined.png" width={650} height={450} />
+        </div>
       </div>
-    </div>
-    <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
-      <div>
-        <h2 className="text-2xl">Joined Demo</h2>
-        <p>
-          Here you have clicked join room, and another user has also joined (i am meeting with myself in another window).
-          this is what it should look like.
-          <br />
-          The icons down, the mic and the cam, represent's the current state of the participant, to know if the participant is muted, or turns off his/her video.
-          
-          <br />
-          To leave the meeting, just click the red icon, which says leave room, that helps you exit
-        </p>
-      </div>
-      <div>
-        <img src="/callie-joined.png" width={650} height={450} />
-      </div>
-    </div>
     </>
   );
 }
 
-function ChatDemo(){
+function ChatDemo() {
   return (
     <>
-     <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
-       <div>
-        <h2 className="text-2xl">Chat Demo</h2>
-        <p>That's the chat box, where you can send messages to everyone in the meeting and also recieve messages in real time.
-          <br /> Sending messages is relatively easy, just typing your message and hit enter or click the send icon
-        </p>
-       </div>
-       <div>
-        <img src="/callie-chat-2.png" width={650} height={450} />
-       </div>
-     </div>
+      <div className="flex lg:flex-row items-center flex-col text-center justify-evenly p-5 bg-[#31363F] gap-4">
+        <div>
+          <h2 className="text-2xl">Chat Demo</h2>
+          <p>
+            That's the chat box, where you can send messages to everyone in the
+            meeting and also recieve messages in real time.
+            <br /> Sending messages is relatively easy, just typing your message
+            and hit enter or click the send icon
+          </p>
+        </div>
+        <div>
+          <img src="/callie-chat-2.png" width={650} height={450} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 function getOS() {
   const userAgent = window.navigator.userAgent;
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Intro from "../components/Intro";
+import { useState } from "react";
+import { motion } from "framer-motion";
 export default function Root() {
   return (
     <>
@@ -10,17 +12,103 @@ export default function Root() {
     </>
   );
 }
-function Nav(){
+function Nav() {
+  const [open, setOpen] = useState(false);
   return (
     <>
-     <div className="w-full flex items-center fixed bg-[#31363F] justify-between">
+      <div className="fixed flex-col z-10 flex w-full">
+      <div className="w-full flex items-center bg-[#31363F] justify-between">
         <h1 className="text-3xl p-2">Callie</h1>
         <div className="lg:flex md:flex hidden justify-around items-center p-4 gap-4">
-          <a href="/" className="bg-[#76ABAE] p-2">Home</a>
-          <a href="/dl" className="bg-[#76ABAE] p-2">Download</a>
-          <a href="/join" className="bg-[#76ABAE] p-2">Join Meeting</a>
+          <a href="/" className="bg-[#76ABAE] p-2">
+            Home
+          </a>
+          <a href="/dl" className="bg-[#76ABAE] p-2">
+            Download
+          </a>
+          <a href="/join" className="bg-[#76ABAE] p-2">
+            Join Meeting
+          </a>
         </div>
-     </div>
+        <div className="lg:hidden md:hidden p-2">
+          <Determiner open={open} click={() => setOpen(!open)} />
+        </div>
+      </div>
+      {open && <MenuLists />}
+      </div>
     </>
-  )
+  );
+}
+function Determiner({
+  open,
+  click,
+}: {
+  open: boolean;
+  click: React.MouseEventHandler<HTMLDivElement>;
+}) {
+  return (
+    <div onClick={click} className="p-1 border rounded-full border-[#76ABAE]">
+      {open ? <X /> : <Bar />}
+    </div>
+  );
+}
+
+function MenuLists() {
+  return (
+    <>
+      <motion.div
+       initial={{translateY:"-100%", opacity:0.4}}
+       animate={{translateY:0, opacity:1}}
+       transition={{duration:0.5, delay:0.2}}
+      >
+      <div className="flex flex-col bg-[#31363F]">
+        <a href="/" className="bg-[#76ABAE] p-2">
+          Home
+        </a>
+        <a href="/dl" className="bg-[#76ABAE] p-2">
+          Download
+        </a>
+        <a href="/join" className="bg-[#76ABAE] p-2">
+          Join Meeting
+        </a>
+      </div>
+      </motion.div>
+    </>
+  );
+}
+function Bar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+      />
+    </svg>
+  );
+}
+function X() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
 }
