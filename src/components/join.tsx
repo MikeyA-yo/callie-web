bda5f0985c7d613e32791f906e893fa35ee57a3d
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-10-01 08:59:07 +0100

    add a way for generating links

--- src/components/join.tsx
+++ src/components/join.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 export default function Join(){
     const [uname, setUname] = useState("")
     const [roomID, setRoomID] = useState("");
+    const [link, setLink] = useState("");
     return (
         <>
             <div className="bg-[#222831] flex flex-col items-center justify-center h-screen">
@@ -12,8 +13,16 @@ export default function Join(){
                    Room ID:
                    <input className="p-2 rounded text-[#31363F] outline-none focus:border-[0.15rem] focus:border-[#76ABAE]" placeholder="room id or address" onChange={(e) => setRoomID(e.target.value)} />
                    <button className="p-2 border rounded border-[#76ABAE] hover:border-0 hover:bg-[#76ABAE]" onClick={()=>{
-                     location.replace(`/join/${roomID}?uname=${uname}`)
+                     if(roomID.length > 1){
+                        location.replace(`/join/${roomID}?uname=${uname}`)
+                     }else{
+                        alert("Room ID must be greater than 1 character")
+                     }
                    }}>Join</button>
+                    <button className="p-2 border rounded border-[#76ABAE] hover:border-0 hover:bg-[#76ABAE]" onClick={()=>{
+                        setLink(location.href + `${roomID}?uname=${uname}` )
+                    }}>Generate Link (optional)</button>
+                    {link.length > 4 && <a href={link} target="_blank">{link}</a>} 
                 </div>
             </div>
        </>
